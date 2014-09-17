3c618d5c65d46dfaa1b8339bcbf433e495e45caf
Hector Fabio Cadavid Rengifo <hcadavid@Hectors-MacBook-Pro.local>
2014-09-16 20:58:20 -0500

    first revision

---
+++ src/test/java/edu/eci/cosw/polizas/pruebas/PersistenceTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.eci.cosw.polizas.pruebas;
+
+
+
+import edu.eci.cosw.polizas.entidades.TipoPoliza;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author hcadavid
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+//@TransactionConfiguration(transactionManager = "XXXXXXXXX", defaultRollback = true)
+public class PersistenceTest {
+    
+    
+    @Test
+    //@Transactional
+    public void testOne(){
+        
+    }
+    
+    
+}
